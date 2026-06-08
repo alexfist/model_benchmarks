44a76a9f503b63d552d4a1563b88e54eb3fb7f96
Alex <alexsliu2005@gmail.com>
2026-06-08 10:51:09 +0800

    donezo

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
-import torch
-import torch_scatter
-import torch_geometric
-import torch_sparse
-import torch_cluster
-from minimol import Minimol
-print("torch version:", torch.__version__)
-print('cuda available:', torch.cuda.is_available())
-if torch.cuda.is_available():
-    print('gpu:', torch.cuda.get_device_name(0))
-print('All imports successful!')
+from deepmol.compound_featurization import MorganFingerprint
+from deepmol.datasets.datasets import SmilesDataset
+from deepmol.models import SklearnModel
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+smiles =[
+            "CC(=O)Oc1ccccc1C(=O)O",        # Aspirin
+            "CN1C=NC2=C1C(=O)N(C(=O)N2C)C", # Caffeine
+        ]
+labels = [1,0]
+dataset = SmilesDataset(smiles = smiles, y=labels)
+fp = MorganFingerprint(radius=2, size=2048)
+feat_dataset = fp.featurize(dataset)
+clf = RandomForestClassifier();
+m = SklearnModel(model = clf, task = 'classification')
+m.fit(feat_dataset)
+
+proba = m.predict_proba(feat_dataset)
+print(f"proba type: {type(proba)}")
+print(f"proba type: {np.array(proba).shape}")
+print(f"proba value: {proba}")
